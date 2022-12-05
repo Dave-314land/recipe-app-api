f25516fdb0c9b9d561fb842cb226b64b3afafc0a
David Piland <dpil8204@TELETRACKING.COM>
2022-12-05 11:36:08 -0500

    added min length requirement with test

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -2,8 +2,8 @@
 Serializers for the user API View
 """
 from django.contrib.auth import get_user_model
-
 from rest_framework import serializers
+
 
 class UserSerializer(serializers.ModelSerializer):
     """Serializer for the user object"""
@@ -11,7 +11,7 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
         fields = ['email', 'password', 'name']
-        extra_kwargs = {'password': {'write_only': True}}
+        extra_kwargs = {'password': {'write_only': True, 'min_length': 8}}
 
     def create(self, validated_data):
         """Create and return a user with encrypted passwords"""
--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -1,16 +1,15 @@
 """
 Tests for the user API
 """
-
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 from django.urls import reverse
-
 from rest_framework.test import APIClient
 from rest_framework import status
 
 
 CREATE_USER_URL = reverse('user:create')
+
 
 def create_user(**params):
     """Create and return a new user"""
@@ -28,12 +27,12 @@ class PublicUserApiTests(TestCase):
             'name': 'Test name',
         }
         self.response = self.client.post(CREATE_USER_URL, self.payload)
-    
+
     def test_create_user_success(self):
         """Test creating the user is successfull"""
         response = self.response
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-    
+
     def test_user_has_password(self):
         """Test user created with password"""
         payload = self.payload
@@ -44,6 +43,13 @@ class PublicUserApiTests(TestCase):
         """Test response data does not contain password"""
         response = self.response
         self.assertNotIn('password', response.data)
+
+    def test_user_password_meets_minimum_length(self):
+        "Test user password meets minimum length requirement"
+        min_length = 8
+        payload = self.payload
+        user = get_user_model().objects.get(email=payload['email'])
+        self.assertGreaterEqual(len(user.password), min_length,)
 
     def test_user_has_name(self):
         """Test user created with name"""
